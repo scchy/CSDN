0cd52cb776a3fba8bd0bfe15f946bac016fd305f
Scc_hy <34824890+scchy@users.noreply.github.com>
2021-04-08 01:20:34 +0800

    Update 多进程.py

--- 多进程.py
+++ 多进程.py
@@ -1,8 +1,3 @@
-# python3
-# Create date: 2021-04-08
-# Authore: Scc_hy
-# Func: 多进程运行性能比较
-
 
 
 from datetime import datetime 
@@ -17,7 +12,7 @@ import time
 def timer(title):
     st = time.perf_counter()
     yield
-    print(f'{title} - done in {time.perf_counter()-st:.2f}s')
+    print(f'{title} - done in {time.perf_counter()-st:.5f}s')
 
 
 class SomeTry:
@@ -28,6 +23,17 @@ class SomeTry:
         n, m = m_n_tuple
         return self.reduce_multi(n) / self.reduce_multi(m) 
 
+    def C_next(self, m, n):
+        out = self.C(m, n)
+        if out > 10**100:
+            return out
+        return 0
+
+    def C_tuple_next(self, m_n_tuple):
+        out = self.C_tuple(m_n_tuple)
+        if out > 10**100:
+            return out
+        return 0
 
     def A(self, m, n):
         return self.reduce_multi(n) / self.reduce_multi(n-m) 
@@ -42,8 +48,8 @@ class SomeTry:
         """
         t = cls()
         if tuple_flag:
-            return t.C_tuple
-        return t.C
+            return t.C_tuple_next
+        return t.C_next
 
     @staticmethod
     def parallel_func(func_, param_iterator):
@@ -65,17 +71,57 @@ class SomeTry:
     def parallel_C(self, m_list, n_list):
         func_c = self.C_for_parallel(tuple_flag=False)
         func_tuple = self.C_for_parallel(tuple_flag=True)
-        param_iterator = zip(m_list, n_list)
-    
-        res_map = self.parallel_func_map(self.C_tuple, param_iterator)
-        res_task = self.parallel_func(self.C, param_iterator)
-
-        res_map_func = self.parallel_func_map(func_tuple, param_iterator)
-        res_task_func = self.parallel_func(func_c, param_iterator)
+        res_map, res_task, res_map_func, res_task_func = '', '', '', ''
+        res_map = self.parallel_func_map(self.C_tuple_next, zip(m_list, n_list))
+        res_task = self.parallel_func(self.C_next, zip(m_list, n_list))
+        print('--'*25)
+        print('use function')
+        res_map_func = self.parallel_func_map(func_tuple, zip(m_list, n_list))
+        res_task_func = self.parallel_func(func_c, zip(m_list, n_list))
         return list(res_map), list(res_task), list(res_map_func), list(res_task_func)
+
+    def parallel_C2(self, m_list, n_list):
+        func_c = self.C_for_parallel(tuple_flag=False)
+        func_tuple = self.C_for_parallel(tuple_flag=True)
+        res_map, res_task, res_map_func, res_task_func = '', '', '', ''
+        worker = os.cpu_count() * 3 // 5
+        with timer('C_tuple Parallel run.'):
+            with ProcessPoolExecutor(worker) as executor:
+                res_map = executor.map(self.C_tuple_next, zip(m_list, n_list))
+
+        executor = ProcessPoolExecutor(worker)
+        with timer(f'C Parallel run.'):
+            task_list = [executor.submit(self.C_next, *parami) for parami in  zip(m_list, n_list)]
+            res_task = [task.result() for task in as_completed(task_list)]
+
+        print('--'*25)
+        print('use function')
+        res_map_func = self.parallel_func_map(func_tuple, zip(m_list, n_list))
+        res_task_func = self.parallel_func(func_c, zip(m_list, n_list))
+        return list(res_map), list(res_task), list(res_map_func), list(res_task_func)
+
 
 
 if __name__ == "__main__":
     s = SomeTry()
-    a, b, c, d = s.parallel_C(list(range(1, 5000)), list(range(500, 5500)))
-    # print(a)
+    a, b, c, d = s.parallel_C(list(range(1, 100))*120, list(range(50, 150))*120)
+    print('\n','**'*25)
+    a, b, c, d = s.parallel_C2(list(range(1, 100))*120, list(range(50, 150))*120)
+    # print(a, c)
+
+"""
+C_tuple_next Parallel run. - done in 14.85585s
+C_next Parallel run. - done in 15.34117s
+--------------------------------------------------
+use function
+C_tuple_next Parallel run. - done in 16.36587s
+C_next Parallel run. - done in 16.27177s
+
+ **************************************************
+C_tuple Parallel run. - done in 16.23173s
+C Parallel run. - done in 15.66818s
+--------------------------------------------------
+use function
+C_tuple_next Parallel run. - done in 16.43504s
+C_next Parallel run. - done in 15.63722s
+"""
